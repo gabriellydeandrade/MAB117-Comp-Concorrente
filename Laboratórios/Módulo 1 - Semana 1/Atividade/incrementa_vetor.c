3e291434e5657c2ba7140b6bf8072e58b8b935b1
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2020-12-06 20:15:56 -0300

    refactor: adding comments and treatment for lenght of vector

--- Laboratórios/Módulo 1 - Semana 1/Atividade/incrementa_vetor.c
+++ Laboratórios/Módulo 1 - Semana 1/Atividade/incrementa_vetor.c
@@ -1,3 +1,9 @@
+/* Disciplina: Computação Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Módulo 1 - Laboratório: 1 */
+/* Laboratório: Implementar um programa concorrente, com duas threads (alem da thread principal),
+ * para incrementar de 1 cada elemento de um vetor de N (10 < N < 100) elementos. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -22,11 +28,17 @@ void *threadIncremento(void * arg) {
 }
 
 int main() {
-    int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
     int tamanhoVetor = sizeof(vetor) / sizeof(vetor[0]); // Divide o tamanho total do vetor pela quantidade de bits para saber a quantidade de elementos
+    threadArgs *arg;
+    pthread_t threadID[NTHREADS];
 
+    if (tamanhoVetor <= 10 || tamanhoVetor >= 100) {
+        printf("O tamanho do vetor deverá estar em um intervalo de (10 < N < 100) elementos \n");
+        return 0;
+    }
 
-    printf("========= VALORES INICIAIS ========= \n\n");
+    printf("=========== VALORES INICIAIS =========== \n\n");
     for (int i=0; i < tamanhoVetor; i++){
         printf("Valor inicial do vetor na posição %d: %d \n", i, vetor[i]);
     }
@@ -36,13 +48,9 @@ int main() {
     // Determina a quantidade de tasks a serem distribuídas para cada thread
 
     int qtdTasksThread1 = tamanhoVetor / NTHREADS;
-    int qtdTasksThread2 = qtdTasksThread1 + (tamanhoVetor % NTHREADS);
+    // int qtdTasksThread2 = qtdTasksThread1 + (tamanhoVetor % NTHREADS);
+    // printf("Quantidade de tasks a serem atribuídas por threads: %d e %d \n\n", qtdTasksThread1, qtdTasksThread2);
 
-    printf("Quantidade de tasks a serem atribuídas por threads: %d e %d \n\n", qtdTasksThread1, qtdTasksThread2);
-
-    threadArgs *arg;
-
-    pthread_t threadID[NTHREADS];
     for (int i=0; i<NTHREADS; i++){
 
         arg = malloc(sizeof(threadArgs));
@@ -78,7 +86,7 @@ int main() {
         }
     }
 
-    printf("========= VALORES FINAIS ========= \n\n");
+    printf("=========== VALORES FINAIS =========== \n\n");
     for (int i=0; i < tamanhoVetor; i++){
         printf("Valor final do vetor na posição %d: %d \n", i, vetor[i]);
     }
