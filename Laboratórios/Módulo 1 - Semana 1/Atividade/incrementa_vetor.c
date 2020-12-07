8a6226c14b09cdf730e5f19a5028f1d12c0685ef
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2020-12-07 19:47:02 -0300

    refactor: divide tasks in generic way

--- Laboratórios/Módulo 1 - Semana 1/Atividade/incrementa_vetor.c
+++ Laboratórios/Módulo 1 - Semana 1/Atividade/incrementa_vetor.c
@@ -19,6 +19,8 @@ typedef struct {
 void *threadIncremento(void * arg) {
     threadArgs *args = (threadArgs *) arg;
 
+    printf("argumenentos %d %d \n", args->posicaoInicialVetor, args->posicaoFinalVetor);
+
     for (int i=args->posicaoInicialVetor; i<=args->posicaoFinalVetor; i++){
         ++args->vetor[i];
     }
@@ -28,7 +30,7 @@ void *threadIncremento(void * arg) {
 }
 
 int main() {
-    int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+    int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     int tamanhoVetor = sizeof(vetor) / sizeof(vetor[0]); // Divide o tamanho total do vetor pela quantidade de bits para saber a quantidade de elementos
     threadArgs *arg;
     pthread_t threadID[NTHREADS];
@@ -47,10 +49,13 @@ int main() {
 
     // Determina a quantidade de tasks a serem distribuídas para cada thread
 
-    int qtdTasksThread1 = tamanhoVetor / NTHREADS;
-    
-    for (int i=0; i<NTHREADS; i++){
+    int qtdTasks = tamanhoVetor / NTHREADS;
+    int qtdTasksExtras = tamanhoVetor % NTHREADS;
+    threadArgs * vetorThreadArgs[NTHREADS];
+    int posicaoInicial = 0;
+    int posicaoFinal = qtdTasks;
 
+    for (int i=0; i<NTHREADS; i++){
         arg = malloc(sizeof(threadArgs));
         if (!arg) {
             printf("ERRO; não foi possível alocar memória\n");
@@ -58,16 +63,24 @@ int main() {
         }
 
         arg->vetor = vetor;
+        arg->posicaoInicialVetor = posicaoInicial;
 
-        if (i==0){
-            arg->posicaoInicialVetor = 0;
-            arg->posicaoFinalVetor = qtdTasksThread1-1;
+        if (qtdTasksExtras){
+            arg->posicaoFinalVetor = (qtdTasks + qtdTasksExtras) - 1;
+            qtdTasksExtras = 0;
         } else {
-            arg->posicaoInicialVetor = qtdTasksThread1;
-            arg->posicaoFinalVetor = tamanhoVetor - 1;
+            arg->posicaoFinalVetor = posicaoFinal - 1;
         }
 
-        int statusPthreadCreate = pthread_create(&threadID[i], NULL, threadIncremento, (void *) arg);
+        vetorThreadArgs[i] = arg;
+
+        posicaoInicial = arg->posicaoFinalVetor + 1;
+        posicaoFinal = posicaoInicial + qtdTasks;
+    }
+
+
+    for (int i=0; i<NTHREADS; i++){
+        int statusPthreadCreate = pthread_create(&threadID[i], NULL, threadIncremento, (void *) vetorThreadArgs[i]);
         if (statusPthreadCreate) {
             printf("ERRO; status code retornado no pthread_create() = %d\n", statusPthreadCreate);
             exit(-1);
