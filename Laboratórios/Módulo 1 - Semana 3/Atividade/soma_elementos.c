a48cb77b54bc93b2d42f78d2e8daea90e6ec3982
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2020-12-19 16:36:37 -0300

    M1-S3-Aula-1 : adiciona soma_elementos concorrente em precisão simples (float)

--- Laboratórios/Módulo 1 - Semana 3/Atividade/soma_elementos.c
+++ Laboratórios/Módulo 1 - Semana 3/Atividade/soma_elementos.c
@@ -4,19 +4,18 @@
 #include "timer.h"
 
 int qtd_threads;
-int *vetor;
+float *vetor;
 long int dimensao_vetor;
 
 void inicializa_e_preenche_vetor();
 void *tarefa(void *arg);
-void libera_vetor();
 
 int main(int argc, char *argv[]){
     double tempo_inicio, tempo_fim;
-    long int soma_sequencial = 0;
-    long int soma_concorrente = 0;
+    float soma_sequencial = 0;
+    float soma_concorrente = 0;
     pthread_t *thread_id;
-    long int soma_retornada_thread;
+    float *soma_retornada_thread;
 
     if (argc < 3){
         fprintf(stderr,"Informe a dimensão do vetor e o número de threads do programa <%s> por argumento \n", argv[0]);
@@ -65,22 +64,24 @@ int main(int argc, char *argv[]){
             return 2;
         }
 
-        soma_concorrente += soma_retornada_thread;
+        soma_concorrente += *soma_retornada_thread;
+        free(soma_retornada_thread);
     }
 
     GET_TIME(tempo_fim);
     printf("Tempo concorrente: %lf\n", tempo_fim - tempo_inicio);
 
 
-    printf("Soma sequencial: %ld\n", soma_sequencial);
-    printf("Soma concorrente: %ld\n", soma_concorrente);
-    libera_vetor();
+    printf("Soma sequencial: %f\n", soma_sequencial);
+    printf("Soma concorrente: %f\n", soma_concorrente);
+
+    free(vetor);
 
     return 0;
 }
 
 void inicializa_e_preenche_vetor(){
-    vetor = (int *) malloc(sizeof(int) * dimensao_vetor);
+    vetor = (float *) malloc(sizeof(float) * dimensao_vetor);
 
     if (!vetor){
         fprintf(stderr, "Não foi possível alocar memória para o vetor usando o malloc \n");
@@ -88,17 +89,24 @@ void inicializa_e_preenche_vetor(){
     }
 
     for (long int i=0; i<dimensao_vetor; i++){
-        vetor[i] = i % 1000;
+        vetor[i] = 1000.1/(i+1);
     }
 
 }
 
 void *tarefa(void *arg){
     long int id = (long int) arg;
-    long int soma_local = 0;
+    float *soma_local;
     long int tamanho_bloco = dimensao_vetor / qtd_threads;
     long int inicio_bloco = id * tamanho_bloco;
     long int fim_bloco;
+
+    soma_local = (float *) malloc(sizeof(float));
+    if (!soma_local){
+        fprintf(stderr, "Não foi possível alocar memória para a soma local usando o malloc \n");
+        exit(1);
+    }
+
 
     if (id == qtd_threads - 1){
         fim_bloco = dimensao_vetor;
@@ -108,13 +116,9 @@ void *tarefa(void *arg){
     }
 
     for (int i=inicio_bloco; i<fim_bloco; i++){
-        soma_local += vetor[i];
+        *soma_local += vetor[i];
     }
 
     pthread_exit((void *) soma_local);
 
-}
-
-void libera_vetor(){
-    free(vetor);
 }
