922c88144ac599795f06b4e310dd9995a10e0278
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2021-01-06 00:58:29 -0300

    M1-T1 - Adiciona caso de teste (TODO)
    #TODO investigar caso de ao simular erro no código principal não refletir nos testes

--- Trabalho de Implementação/Módulo 1/teste_merge_sort_concorrente.c
+++ Trabalho de Implementação/Módulo 1/teste_merge_sort_concorrente.c
@@ -88,15 +88,37 @@ void testa_se_ordena_vetor_ordenado_em_ordem_decrescente(){
         puts(ANSI_COR_FALHA"RESULTADO" ANSI_COR_PADRAO);
         imprime_vetor(tamanho_vetor, resultado);
     }
+}
 
+void testa_se_ordena_corretamente_independente_da_quantidade_de_threads(){
+    tamanho_vetor = 10;
+    int vetor[] = {5, 3, 9, 1, 0, 2, 6, 7, 4, 8};
+    int vetor_esperado[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    for (qtd_threads=1; qtd_threads<tamanho_vetor; qtd_threads++){
+        int *resultado = merge_sort(tamanho_vetor, vetor, qtd_threads);
 
+        if (vetores_iguais(resultado, vetor_esperado))
+            printf(ANSI_COR_SUCESSO "Teste `testa_se_ordena_corretamente_independente_da_quantidade_de_threads` passou \n" ANSI_COR_PADRAO);
+        else{
+            printf(ANSI_COR_FALHA "Teste `testa_se_ordena_corretamente_independente_da_quantidade_de_threads` falhou \n" ANSI_COR_PADRAO);
+
+            puts(ANSI_COR_FALHA "VETOR ESPERADO" ANSI_COR_PADRAO);
+            imprime_vetor(tamanho_vetor, vetor_esperado);
+
+            puts(ANSI_COR_FALHA"RESULTADO" ANSI_COR_PADRAO);
+            imprime_vetor(tamanho_vetor, resultado);
+
+            printf(ANSI_COR_FALHA "QTD DE THREADS: %d \n", qtd_threads);
+        }
+    }
 }
 
 int teste() {
     testa_se_retorna_vetor_ordenado();
     testa_se_retorna_vetor_igual_se_ele_ja_for_ordenado();
     testa_se_ordena_vetor_ordenado_em_ordem_decrescente();
+    testa_se_ordena_corretamente_independente_da_quantidade_de_threads();
 
     return 0;
 }
